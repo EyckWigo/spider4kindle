b2dbd09cd14ea8a33f50d513cab4302a37a71bbf
Runzhen Wang <runzhew@g.clemson.edu>
2015-11-08 10:59:36 -0800

    spider4kindle.sh

--- spider4kindle.sh
+++ spider4kindle.sh
@@ -1,3 +1,7 @@
+#!/bin/bash 
 
+scrapy crawl news
 
 ebook-convert result.html cnbeta.mobi --output-profile kindle --no-inline-toc --title "news" --publisher 'anoy' --language en --authors 'spider4kindle'
+
+rm -f result.html
